1dae08ca3df2d873c69a9086bb52b27387903c51
tre3ubec <ekoroteev27@yandex.ru>
2022-05-11 22:33:42 +0300

    added Portfolio Page

--- src/components/page/PortfolioPage/PortfolioPage.js
+++ src/components/page/PortfolioPage/PortfolioPage.js
@@ -2,7 +2,13 @@ import './PortfolioPage.css'
 
 const PortfolioPage = () => (
   <div className={"PortfolioPage"}>
-    Portfolio Page!
+    <h1 className={"PageTitle"}>Portfolio</h1>
+    <span className={"PageDesc"}>My recent Work</span>
+    <div className="slider">There will slider!</div>
+    <footer>
+      <h2 className={"FooterHeader"}>You have a new project?</h2>
+      <p className={"FooterDesc"}>Contact me now nad get special price on your project</p>
+    </footer>
   </div>
 )
 
